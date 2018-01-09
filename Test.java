392b4ab2d5fb4a9479d373b6eb239bae9a5831c9
Ogomb <lewis.mbogo@andela.com>
2018-01-09 11:43:17 +0300

    hello world program

---
+++ Test.java
@@ -0,0 +1,19 @@
+/* Hello world program 
+
+ The name of the file needs to bear the name Test which is the class name, the contents of a class
+ are wrapped within two curly braces.
+*/
+
+public class Test {
+
+  /* Every java application needs to have the main method, it is where the execution of the program begins.
+  the contents of a method are wrapped within curly braces.
+  */
+  public static void main(String [] args) {
+  
+    //the next line of code is a statement that outputs 'Hello world!!' to the console
+    
+    System.out.print("Hello world!!);
+  }
+  
+}
